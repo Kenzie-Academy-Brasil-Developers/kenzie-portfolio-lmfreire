a456a314244c517f3faebe4ae04711e9701a7c49
lmfreire <l.matheus.freire10@gmil.com>
2022-07-26 11:21:39 -0300

    feat: adicionado meus dados

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Matheus Freire",
+  githubUser: "lmfreire",
+  linkedinUser: "lucas-matheus-freire-da-silva-6342a7166",
+  whatsappNumber: "+5584991581521",
 };
